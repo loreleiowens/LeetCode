33206c0f36022bec0bb5fd6e8f3fe0867f2548c4
Lorelei <loreleimowens@gmail.com>
2025-04-30 22:19:01 +0930

    Corrections to work
    Updating to make corrections via the help of NeetCode - initially very close to the answer, just needed the correct if-statement

--- Easy/LeetCode 13 - Roman to Integer.py
+++ Easy/LeetCode 13 - Roman to Integer.py
@@ -1,18 +1,12 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         # Dictionary of all the various symbols & number equiv.
-        numDict = {
-            'I':1, 'IV':4, 'V':5,
-            'IX':9, 'X':10,
-            'XL':40, 'L':50,
-            'XC':90, 'C':100,
-            'CD':400, 'D':500,
-            'CM':900, 'M':1000
-        }
-
+        numDict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
         num = 0
-        
-        for char in s:
-            if char in numDict:
-                num += numDict[char]
+
+        for i in range(len(s)):
+            if i+1 < len(s) and numDict[s[i]] < numDict[s[i+1]]:
+                num -= numDict[s[i]]
+            else:
+                num += numDict[s[i]]
         return num

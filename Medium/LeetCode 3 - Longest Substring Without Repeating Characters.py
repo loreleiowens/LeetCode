e2721825fbfd96276187ac659610b2a14737d785
Lorelei <loreleimowens@gmail.com>
2025-05-01 23:00:18 +0930

    Explaining the solution
    Whenever a duplicate is found, it is removed, pushes the entire left pointer over one then adds the right pointer value in (duplicate) and updates maxLen

--- Medium/LeetCode 3 - Longest Substring Without Repeating Characters.py
+++ Medium/LeetCode 3 - Longest Substring Without Repeating Characters.py
@@ -10,3 +10,17 @@ class Solution:
         #         maxLen += 1
             
         # return maxLen
+
+     # Proper solution w/ comments
+        l = maxLen = 0
+        charSet = set()
+        
+        for r in range(len(s)):
+            while s[r] in charSet: 
+                charSet.remove(s[l]) # Remove duplicate & push sliding window over one
+                l += 1
+
+            charSet.add(s[r]) # Checks if r is in charSet - adds back in duplicate value as sliding window updated to exclude
+            maxLen = max(maxLen, r - l + 1) # Updates maxLen everytime unique value appears
+        
+        return maxLen
